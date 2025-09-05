57f65a0c0943e85e6c8d4f8398f6a87dacb32c75
Abhiram <davalaabhiram71@gmail.com>
2025-09-05 20:34:39 +0530

    update index.html and main.jsx  for css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
-
+import "./index.css"; // Import CSS here to avoid 404s
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <AuthProvider>
